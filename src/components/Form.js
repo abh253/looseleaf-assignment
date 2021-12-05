e1005e5f2b76efb39111c1990fe72a47d7f976bc
abh253 <abhisheksingh2532001@gmail.com>
2021-12-06 01:17:50 +0530

    page2 final

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,15 +8,13 @@ const Form = () => {
         <section className='m-3 mx-4 pt-5 mt-0' style={{color:'white'}}>
             <h1 className='fs-2 text-center'>Contact us to get Started.</h1>
             <form action="#" onSubmit={(e) => { e.preventDefault() }}>
-                <div className="row flex-column align-content-center mt-5">
+                <div className="row flex-column align-items-center mt-5">
                     {
                         inputAreas.map((single, index) => {
-                            return <input key={index} type="text" name={single} placeholder={single} className='my-2 border border-2 fw-bold p-2 border-white bg-dark col-md-6 rounded' />
+                            return <input key={index} type="text" name={single} placeholder={single} className='my-2 border border-2 p-2 border-white bg-dark col-md-6 rounded text-white' />
                         })
                     }
-                    <Link to='/contact'>
-                    <button className="btn btn-primary col-4 col-md-3 m-auto mt-3" type='button'>Contact us</button>
-                    </Link>
+                    <Link to='/contact' className="btn btn-primary col-4 col-md-3 mt-3 align-self-center" >Contact us</Link>
                 </div>
             </form>
         </section>
--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import Query from '../components/Query'
 const Page2 = () => {
+    const queries = [
+        'Why Looseleaf?', 'Features', 'Pricing', 'Blogs', 'Contact No'
+    ]
     return (
-        <div>
-            hello from page2
-        </div>
+        <section className='mx-3'>
+            <article className='d-flex justify-content-between align-items-center border-bottom border-4 pb-2 my-3'>
+                <p className='fs-5 mb-0 fw-bold'>looseleaf</p>
+                <button type='button' className='btn btn-primary text-uppercase fs-8 '>Launch Looseleaf</button>
+            </article>
+            <article>
+                {
+                    queries.map((single, index) => {
+                        return <div className='border-bottom border-2 pt-2'><Query question={single} key={index} /></div>;
+                    })
+                }
+            </article>
+            <article className='d-flex flex-column mt-5'>
+                <button type='button' className='text-uppercase btn btn-white rounded border border-4 border-primary text-primary my-2 fs-3 fw-bold'>Talk to sales</button>
+                <button type='button' className='text-uppercase btn btn-primary rounded my-1 fs-3 fw-bold my-2'>Launch Looseleaf</button>
+            </article>
+        </section>
     )
 }
 
